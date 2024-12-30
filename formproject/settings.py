6331983a4c737b0fddb5198074ae272460a33779
saurabh-Radke <saurabh.radke@dialectai.tech>
2024-12-31 00:42:54 +0530

    delpy2

--- formproject/settings.py
+++ formproject/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = 'django-insecure-_#(7-y@!#os1_o#%u*k74)7v+%=4nx0udxrww*p@=*e(uzf)!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
